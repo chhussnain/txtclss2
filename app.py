a8507cbe6c4a3f683f3c7264f65b86c596e12a23
chhussnain <126756759+chhussnain@users.noreply.github.com>
2023-11-16 23:58:39 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,22 @@
+import streamlit as st
 from transformers import pipeline
-import gradio as gr
 
 model_checkpoint = "MuntasirHossain/distilbert-finetuned-ag-news"
 model = pipeline("text-classification", model=model_checkpoint)
 
-
 def classify(text):
-    label = model(text)[0]["label"]
-    return label
+    result = model(text)[0]
+    label = result["label"]
+    score = round(result["score"], 4)
+    return f"{label} (Score: {score})"
 
-description = "This AI model is trained to classify texts/articles/sentences into four categories: World, Sports, Business and Science/Tech." 
-title = "Classify Your Texts"
-theme = "peach"
-examples=[["Global Retail Giants Gear Up for Record-Breaking Holiday Sales Season Amidst Supply Chain Challenges and Rising Consumer Demand."]]
-
-gr.Interface(fn=classify,
-    inputs="textbox",
-    outputs="text",
-    title=title,
-    theme = theme,
-    description=description,
-    examples=examples,
-).launch()
+# Streamlit app
+st.title("Classify Your Texts")
+st.sidebar.write("This AI model is trained to classify texts/articles/sentences into four categories: World, Sports, Business, and Science/Tech.")
+text_input = st.text_area("Enter your text here:")
+if st.button("Classify"):
+    if text_input:
+        result = classify(text_input)
+        st.write(result)
+    else:
+        st.warning("Please enter text for classification.")
